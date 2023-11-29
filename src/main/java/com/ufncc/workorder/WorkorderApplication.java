d452c911d737e11a38725436357ff90e5e5ab8f3
luiz-bcardoso <luizbatistacardoso@gmail.com>
2023-11-28 23:34:31 -0300

    Created initial project, added dependencies and configured MySQL connection.

---
+++ src/main/java/com/ufncc/workorder/WorkorderApplication.java
@@ -0,0 +1,13 @@
+package com.ufncc.workorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufncc/workorder/WorkorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufncc.workorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
